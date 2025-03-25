0e94ae7668cf6f1c98600b88482840d88211b11a
suhyeon lee <tngus0673@naver.com>
2025-03-25 15:35:03 +0900

    feat: Entity - DataError 구현

---
+++ suhyeon/HarryPotterBooks/HarryPotterBooks/Domain/Entity/DataError.swift
@@ -0,0 +1,25 @@
+//
+//  DataError.swift
+//  HarryPotterBooks
+//
+//  Created by 이수현 on 3/25/25.
+//
+
+import Foundation
+
+enum DataError: Error {
+    case fileNotFound   // 데이터를 찾을 수 없을 때
+    case parsingFaild   // 데이터 파싱에 실패했을 때
+}
+
+
+extension DataError {
+    var description: String {
+        switch self {
+        case .fileNotFound:
+            "파일을 찾을 수 없습니다"
+        case .parsingFaild:
+            "데이터 파싱에 실패했습니다"
+        }
+    }
+}
