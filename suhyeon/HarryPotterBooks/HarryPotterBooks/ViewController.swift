f6fbb7d8e65ef7e93fdda7efcc09367326b91174
suhyeon lee <tngus0673@naver.com>
2025-03-25 14:50:36 +0900

    chore: 프로젝트 세팅
    1. 코드베이스 설정
    2. 이미지, data.json 파일 추가
    3. 라이브러리 설치 (Then, SnapKit, RxSwift)

---
+++ suhyeon/HarryPotterBooks/HarryPotterBooks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HarryPotterBooks
+//
+//  Created by 이수현 on 3/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
