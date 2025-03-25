e1ede6b3f4155394f7588b0ef5dc9883d0e07d5a
youseokhwan <youseokhwan15@gmail.com>
2025-03-25 16:07:25 +0900

    refactor: 개행 수정

--- seokhwan/HarryPotterBooks/HarryPotterBooks/AppDelegate.swift
+++ seokhwan/HarryPotterBooks/HarryPotterBooks/AppDelegate.swift
@@ -14,6 +14,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
     }
 }
--- seokhwan/HarryPotterBooks/HarryPotterBooks/ViewController.swift
+++ seokhwan/HarryPotterBooks/HarryPotterBooks/ViewController.swift
@@ -4,26 +4,31 @@ import SnapKit
 final class ViewController: UIViewController {
     private lazy var bookTitleLabel: UILabel = {
         let label = UILabel()
+
         label.text = "HarryPotterBooks"
         label.textAlignment = .center
         label.font = .boldSystemFont(ofSize: 24)
+
         return label
     }()
-
     private lazy var seriesNumberButtonsStackView: UIStackView = {
         let stackView = UIStackView()
+
         stackView.axis = .horizontal
+
         return stackView
     }()
-
     private lazy var seriesNumberButton: UIButton = {
         var container = AttributeContainer()
         container.font = UIFont.systemFont(ofSize: 16)
+
         var configuration = UIButton.Configuration.filled()
         configuration.attributedTitle = AttributedString("1", attributes: container)
+
         let button = UIButton(configuration: configuration)
         button.layer.cornerRadius = 22
         button.layer.masksToBounds = true
+
         return button
     }()
 
