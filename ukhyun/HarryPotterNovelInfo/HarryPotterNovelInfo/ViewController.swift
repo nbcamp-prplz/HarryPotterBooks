d4cae22b39d0525d213633912ffa7466c0f574aa
imo2k <dnrwk99@gmail.com>
2025-03-25 11:04:01 +0900

    chore: Initial Project 생성

---
+++ ukhyun/HarryPotterNovelInfo/HarryPotterNovelInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HarryPotterNovelInfo
+//
+//  Created by GO on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
