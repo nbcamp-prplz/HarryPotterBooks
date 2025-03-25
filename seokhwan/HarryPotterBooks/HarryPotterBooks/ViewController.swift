a26876d68a59ac321fd90b54a25423ac959dfa4b
youseokhwan <youseokhwan15@gmail.com>
2025-03-25 15:53:22 +0900

    feat: 메인 화면 UI 구현

--- seokhwan/HarryPotterBooks/HarryPotterBooks/ViewController.swift
+++ seokhwan/HarryPotterBooks/HarryPotterBooks/ViewController.swift
@@ -1,7 +1,64 @@
 import UIKit
+import SnapKit
 
 final class ViewController: UIViewController {
+    private lazy var bookTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "HarryPotterBooks"
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 24)
+        return label
+    }()
+
+    private lazy var seriesNumberButtonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        return stackView
+    }()
+
+    private lazy var seriesNumberButton: UIButton = {
+        var container = AttributeContainer()
+        container.font = UIFont.systemFont(ofSize: 16)
+        var configuration = UIButton.Configuration.filled()
+        configuration.attributedTitle = AttributedString("1", attributes: container)
+        let button = UIButton(configuration: configuration)
+        button.layer.cornerRadius = 22
+        button.layer.masksToBounds = true
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+    }
+}
+
+private extension ViewController {
+    func configure() {
+        configureSubviews()
+        configureConstraints()
+    }
+
+    func configureSubviews() {
+        [seriesNumberButton].forEach {
+            seriesNumberButtonsStackView.addArrangedSubview($0)
+        }
+        [bookTitleLabel, seriesNumberButtonsStackView].forEach {
+            view.addSubview($0)
+        }
+    }
+
+    func configureConstraints() {
+        bookTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.directionalHorizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        seriesNumberButtonsStackView.snp.makeConstraints { make in
+            make.top.equalTo(bookTitleLabel.snp.bottom).offset(16)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+        }
+        seriesNumberButton.snp.makeConstraints { make in
+            make.size.equalTo(44)
+        }
     }
 }
