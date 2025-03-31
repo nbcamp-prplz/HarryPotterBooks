83331c8ace8243b9ff1aa0a3528268f52e7dd4dd
youseokhwan <youseokhwan15@gmail.com>
2025-03-31 15:43:01 +0900

    refactor: Test를 위한 AppStatesStorage 수정
    UserDefaults를 외부에서 의존성 주입할 수 있도록 수정
    Mock을 위한 AppStatesStorageProtocol 정의 및 채택

--- seokhwan/HarryPotterBooks/HarryPotterBooks/Storage/AppStatesStorage.swift
+++ seokhwan/HarryPotterBooks/HarryPotterBooks/Storage/AppStatesStorage.swift
@@ -1,26 +1,39 @@
 import Foundation
 
-final class AppStatesStorage {
-    enum KeyType: String {
-        case isExpanded
-        case lastSelectedSeriesNumber
+enum UserDefaultsKeyType: String {
+    case isExpanded
+    case lastSelectedSeriesNumber
+}
+
+protocol AppStatesStorageProtocol {
+    func isExpanded(at seriesNumber: Int) -> Bool
+    func updateIsExpanded(at seriesNumber: Int, value: Bool)
+    func lastSelectedSeriesNumber() -> Int
+    func updateLastSelectedSeriesNumber(to seriesNumber: Int)
+}
+
+final class AppStatesStorage: AppStatesStorageProtocol {
+    private let userDefaults: UserDefaults
+
+    init(_ userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
     }
 
     func isExpanded(at seriesNumber: Int) -> Bool {
         let key = userDefaultsKey(.isExpanded, at: seriesNumber)
 
-        return UserDefaults.standard.bool(forKey: key)
+        return userDefaults.bool(forKey: key)
     }
 
     func updateIsExpanded(at seriesNumber: Int, value: Bool) {
         let key = userDefaultsKey(.isExpanded, at: seriesNumber)
 
-        UserDefaults.standard.set(value, forKey: key)
+        userDefaults.set(value, forKey: key)
     }
 
     func lastSelectedSeriesNumber() -> Int {
         let key = userDefaultsKey(.lastSelectedSeriesNumber)
-        let value = UserDefaults.standard.integer(forKey: key)
+        let value = userDefaults.integer(forKey: key)
 
         return value == 0 ? 1 : value
     }
@@ -28,10 +41,10 @@ final class AppStatesStorage {
     func updateLastSelectedSeriesNumber(to seriesNumber: Int) {
         let key = userDefaultsKey(.lastSelectedSeriesNumber)
 
-        return UserDefaults.standard.set(seriesNumber, forKey: key)
+        return userDefaults.set(seriesNumber, forKey: key)
     }
 
-    private func userDefaultsKey(_ type: AppStatesStorage.KeyType, at index: Int? = nil) -> String {
+    private func userDefaultsKey(_ type: UserDefaultsKeyType, at index: Int? = nil) -> String {
         let postfix = index == nil ? "" : "at\(index!)"
 
         return "\(type.rawValue)\(postfix)"
