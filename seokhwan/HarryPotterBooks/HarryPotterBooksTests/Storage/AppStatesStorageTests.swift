2a63fd0d220a6f39470bc518604cdac7ee5f0686
youseokhwan <youseokhwan15@gmail.com>
2025-03-31 16:08:50 +0900

    test: AppStatesStorage 테스트 작성

---
+++ seokhwan/HarryPotterBooks/HarryPotterBooksTests/Storage/AppStatesStorageTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+
+final class AppStatesStorageTests: XCTestCase {
+    var userDefaults: UserDefaults!
+    var storage: AppStatesStorageProtocol!
+
+    override func setUpWithError() throws {
+        userDefaults = UserDefaults.init(suiteName: "Test")!
+        userDefaults.removePersistentDomain(forName: "Test")
+        storage = AppStatesStorage(userDefaults)
+    }
+
+    override func tearDownWithError() throws {
+        userDefaults.removePersistentDomain(forName: "Test")
+        userDefaults = nil
+        storage = nil
+    }
+
+    func testIsExpanded() throws {
+        XCTAssertEqual(storage.isExpanded(at: 3), false)
+
+        storage.updateIsExpanded(at: 3, value: true)
+        XCTAssertEqual(storage.isExpanded(at: 3), true)
+    }
+
+    func testLastSelectedSeriesNumber() throws {
+        XCTAssertEqual(storage.lastSelectedSeriesNumber(), 1)
+
+        storage.updateLastSelectedSeriesNumber(to: 6)
+        XCTAssertEqual(storage.lastSelectedSeriesNumber(), 6)
+    }
+}
