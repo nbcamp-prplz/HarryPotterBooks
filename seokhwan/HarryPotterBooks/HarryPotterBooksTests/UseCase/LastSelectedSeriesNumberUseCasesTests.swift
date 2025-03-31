9a4ba04fe56d538fb8a7f7b4c606466466a46575
youseokhwan <youseokhwan15@gmail.com>
2025-03-31 17:57:05 +0900

    test: LastSelectedSeriesNumberUseCases 테스트 작성

---
+++ seokhwan/HarryPotterBooks/HarryPotterBooksTests/UseCase/LastSelectedSeriesNumberUseCasesTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+
+final class LastSelectedSeriesNumberUseCasesTests: XCTestCase {
+    var mockStorage: AppStatesStorageProtocol!
+    var fetchLastSelectedSeriesNumberUseCase: FetchableLastSelectedSeriesNumberUseCase!
+    var updateLastSelectedSeriesNumberUseCase: UpdatableLastSelectedSeriesNumberUseCase!
+
+    override func setUpWithError() throws {
+        mockStorage = MockAppStatesStorage()
+        fetchLastSelectedSeriesNumberUseCase = FetchLastSelectedSeriesNumberUseCase(appStatesStorage: mockStorage)
+        updateLastSelectedSeriesNumberUseCase = UpdateLastSelectedSeriesNumberUseCase(appStatesStorage: mockStorage)
+    }
+
+    override func tearDownWithError() throws {
+        updateLastSelectedSeriesNumberUseCase = nil
+        fetchLastSelectedSeriesNumberUseCase = nil
+        mockStorage = nil
+    }
+
+    func testExecutes() throws {
+        XCTAssertEqual(fetchLastSelectedSeriesNumberUseCase.execute(), 1)
+
+        updateLastSelectedSeriesNumberUseCase.execute(to: 5)
+        XCTAssertEqual(fetchLastSelectedSeriesNumberUseCase.execute(), 5)
+    }
+}
